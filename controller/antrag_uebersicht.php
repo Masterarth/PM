6f1a11d3f2eebdb7f14d967037babaa25f9711d4
Artur Stalbaum <stalbaum.artur@gmail.com>
2016-07-16 18:44:42 +0200

    - bsp anträge übersicht überarbeitet

--- controller/antrag_uebersicht.php
+++ controller/antrag_uebersicht.php
@@ -8,7 +8,7 @@ for ($index = 1; $index <= 10; $index++) {
     $projekt->setBeschreibung("bla bla");
     $projekt->setBudget(rand(500, 100000));
     $projekt->setStarttermin(date("c"));
-    $projekt->setEndtermin(date("c", strtotime("+" . rand(0, 10) . "day")));
+    $projekt->setEndtermin(date("c", strtotime("+" . rand(1, 10) . "day")));
     $projekt->setMitarbeiteranzahl(rand(1, 10));
     $projekt->setNutzen("gibt keinen ...");
     $projekte[] = $projekt;
