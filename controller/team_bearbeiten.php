bb406d14a72acdb6b8fb3b6b06906369833ab8e4
Dell <Dell@Dell-PC>
2016-08-04 18:51:29 +0200

    Add Changes

--- controller/team_bearbeiten.php
+++ controller/team_bearbeiten.php
@@ -5,13 +5,9 @@ if (isset($_POST["reg"])) {
     $result = core()->db()->select("select * from team where id ='" . $_POST["reg"]["id"] . "'", "fetch");
     if ($result) {
 
-        $team["name"] = $_POST["reg"]["s_name"];
-        $team["plz"] = $_POST["reg"]["plz"];
-        $team["ort"] = $_POST["reg"]["ort"];
-        $team["strasse"] = $_POST["reg"]["strasse"];
-        $team["hausnr"] = $_POST["reg"]["hausnr"];
+        $team["t_name"] = $_POST["reg"]["t_name"];
 
-        $id = core()->db()->update("update standort set t_name=:name, plz=:plz, ort=:ort, strasse=:strasse, hausnummer=:hausnr where id=" . $_POST["reg"]["id"], $standort);
+        $id = core()->db()->update("update standort set t_name=:name where id=" . $_POST["reg"]["id"], $standort);
         header('Location: /pm/team/' . $_POST["reg"]["id"]);
         exit;
     }
