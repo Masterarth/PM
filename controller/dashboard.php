e03ec639113a213cbcc679e8544c4d03188dad42
Dell <Dell@Dell-PC>
2016-09-08 20:08:28 +0200

    Add Changes

--- controller/dashboard.php
+++ controller/dashboard.php
@@ -17,9 +17,11 @@ $antraege_abgelehnt = core()->db()->select("select count(id) from projekt where 
 core()->smarty()->assign("anzahl_abgelehnter_antraege", $antraege_abgelehnt["count(id)"]);
 
 $zu_gehnemigen = core()->db()->select("select projekt.id, projekt.titel FROM projekt,team,abteilung,mitarbeiter, standort, projstatus "
-        . "WHERE (projekt.genehmigung_E1 = 0 AND projekt.a_id = abteilung.id AND abteilung.id = team.a_id AND team.t_leitung = " . $user->getId() . " AND projekt.s_id = 1) "
+        . "WHERE (projekt.genehmigung_E1 = 0 AND projekt.a_id = abteilung.id AND abteilung.a_leitung = " . $user->getId() . " AND projekt.s_id =1) "
         . "OR ( projekt.genehmigung_E2 = 0 AND projekt.a_id = abteilung.id AND abteilung.a_leitung = " . $user->getId() . " AND projekt.s_id =1) "
-        . "OR ( projekt.genehmigung_E3 = 0 AND projekt.a_id = abteilung.id AND abteilung.s_id = standort.id AND standort.s_leitung = " . $user->getId() . " AND projekt.s_id = 1) group by projekt.id");
+        . "OR ( projekt.genehmigung_E3 = 0 AND projekt.a_id = abteilung.id AND abteilung.s_id = standort.id AND standort.s_leitung = " . $user->getId() . " AND projekt.s_id = 1) GROUP BY projekt.id");
+
+//var_dump($zu_gehnemigen);
 
 if (count($zu_gehnemigen) > 0) {
     core()->smarty()->assign("zu_genehmigen", $zu_gehnemigen);
